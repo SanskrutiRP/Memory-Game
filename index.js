a296bda530ded2f4181ca68d64ec60fb0ec7c759
Sanskruti <sanskrutipunde501@gmail.com>
2021-09-21 14:21:58 +0530

    Add files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//fetch the container 
+//fetch the all required fields
 let grid = document.getElementById('grid');
 let min = document.getElementById('minutes');
 let sec = document.getElementById('seconds');
@@ -19,13 +19,13 @@ while(i < 25){
 		grid.appendChild(block);
 	}
 }
-let x ;
+
+let x;
 let blocks = document.querySelectorAll('.blocks');
 let starttime = 0, endtime = 0;
 document.getElementById('start').addEventListener('click',()=>{
 	starttime = new Date();	
 	blocks.forEach((ele)=>{
-		/*ele.style.transform = 'rotate(360deg)';*/
 		ele.innerText = ' ';
 		ele.style.backgroundColor = 'rgba(255, 255, 255, .7)';
 	});
@@ -39,34 +39,26 @@ document.getElementById('start').addEventListener('click',()=>{
 	  	sec.innerText = seconds;
 	},1000);
 	blocks.forEach((ele)=>{
-	ele.addEventListener('click', ()=>{
-		let id = ele.getAttribute("id");
-		if(ele.classList.contains('done') && id == '25'){
-			clearInterval(x);
-			x= 0;
-			min.style.backgroundColor = 'white';
-			min.style.color = 'black';
-			sec.style.backgroundColor = 'white';
-			sec.style.color = 'black';	
-			ele.style.transform = 'rotate(360deg)';
-			ele.innerText = id;
-			ele.style.backgroundColor = 'blueviolet';
-		}
-		else if(ele.classList.contains('done')){
-			ele.style.transform = 'rotate(360deg)';
-			ele.innerText = id;
-			ele.style.backgroundColor = 'blueviolet';
-			let newid = parseInt(id)+1;
-			document.getElementById(newid).classList.add('done');
-		}
-		
-	})
-	})
+		ele.addEventListener('click', ()=>{
+			let id = ele.getAttribute("id");
+			if(ele.classList.contains('done') && id == '25'){
+				clearInterval(x);
+				x = 0;
+				min.style.backgroundColor = 'white';
+				min.style.color = 'black';
+				sec.style.backgroundColor = 'white';
+				sec.style.color = 'black';	
+				ele.style.transform = 'rotate(360deg)';
+				ele.innerText = id;
+				ele.style.backgroundColor = 'blueviolet';
+			}
+			else if(ele.classList.contains('done')){
+				ele.style.transform = 'rotate(360deg)';
+				ele.innerText = id;
+				ele.style.backgroundColor = 'blueviolet';
+				let newid = parseInt(id)+1;
+				document.getElementById(newid).classList.add('done');
+			}
+		});
+	});
 });
-
-
-/*document.getElementById('25').addEventListener('click', ()=>{
-	if(ele.classList.contains('done')){
-		
-	}
-});*/1000
